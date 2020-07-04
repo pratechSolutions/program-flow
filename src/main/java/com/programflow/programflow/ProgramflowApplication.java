7d63cb02d40e4408481a54d3b2a042e981ab0742
zeynel abidin <z.abidintok@gmail.com>
2020-07-04 12:16:09 +0200

    program flow main Project

---
+++ src/main/java/com/programflow/programflow/ProgramflowApplication.java
@@ -0,0 +1,13 @@
+package com.programflow.programflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programflow/programflow/ProgramflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programflow.programflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
