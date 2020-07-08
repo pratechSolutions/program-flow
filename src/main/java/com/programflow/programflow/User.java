328ef669fc1a7a4bc3733336b6515a3437574ded
zeynel abidin <z.abidintok@gmail.com>
2020-07-08 18:13:37 +0200

    second try

---
+++ src/main/java/com/programflow/programflow/User.java
@@ -0,0 +1,20 @@
+package com.programflow.programflow;
+
+import java.time.YearMonth;
+
+public class User {
+
+    public int id;
+    public String name;
+    public String link;
+    public String time;
+
+
+    public User(int id, String name, String link, String time) {
+        this.id = id;
+        this.name = name;
+        this.link = link;
+        this.time = time;
+    }
+}
+
---
+++ src/main/java/com/programflow/programflow/UserController.java
@@ -0,0 +1,27 @@
+package com.programflow.programflow;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping(value = "users/{userId}")
+    public User getUser(@PathVariable int userId){
+
+        User user;
+
+        if (userId==1)
+            user =new User(1,"Alican","wwww.google.com","12.12.2020");
+        else if(userId==2)
+            user=new User(2,"Zeynel","www.google.com","10.08.2020");
+        else
+            user=new User(0,"0","0","0");
+
+
+        return user;
+    }
+
+}
